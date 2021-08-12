ccb0cb43db404b0a674a51a3483473ff6e8e54c3
ReneFuentes26 <fuentesbenitezreneismael@gmail.com>
2021-08-12 10:21:42 -0600

    Actividad termianda

--- Clase-12082021/Colecciones.cs
+++ Clase-12082021/Colecciones.cs
@@ -21,14 +21,27 @@ namespace Clase_12082021
             Students.Add("Miranda Campos", 20);
             Students.Add("Carmen Valladares", 25);
 
+            //leer el diccionario y mostrar los datos
             foreach (KeyValuePair<string, string> employee in EmployeeList)
             {
                 Console.WriteLine("Clave: {0}, Valor: {1}", employee.Key, employee.Value);
             }
 
-            Console.WriteLine("Elementos en el diccionario: {0}", EmployeeList);
+            //ordenar elementos del diccionario
+            var dic = EmployeeList.Keys.ToList();
+            dic.Sort();
+
+            Console.WriteLine("Diccionario ordenado");
+            foreach (var item in dic)
+            {
+                Console.WriteLine("{0}: {1}", item, EmployeeList[item]);
+            }
+            //Contar elementos de un diccionario
+            Console.WriteLine("Elementos en el diccionario: {0}", EmployeeList.Count);
 
             Console.ReadKey();
+
+
         }
     }
 }
---
+++ Clase-12082021/DiccionarioFamilia.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_12082021
+{
+    class DiccionarioFamilia
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> Familia = new Dictionary<string, int>();
+            Familia.Add("René Eduardo Fuentes Treminio", 45);
+            Familia.Add("Flor de Maria Benítez de Fuentes",43);
+            Familia.Add("Eduardo Ernesto Fuentes Benítez", 20);
+            Familia.Add("René Ismael Fuentes Benítez", 18);
+
+            //leer el diccionario y mostrar los datos
+            foreach (KeyValuePair<string, int> familia in Familia)
+            {
+                Console.WriteLine("Clave: {0}, Valor: {1}", familia.Key, familia.Value);
+            }
+
+            //ordenar elementos del diccionario
+            var dic = Familia.Keys.ToList();
+            dic.Sort();
+
+            Console.WriteLine("Diccionario ordenado");
+            foreach (var item in dic)
+            {
+               Console.WriteLine("{0}: {1}", item, Familia[item]);
+            }
+            Console.WriteLine("Elementos en el diccionario: {0}", Familia.Count);
+
+            Console.ReadKey();
+        }
+
+
+        
+    }
+}
